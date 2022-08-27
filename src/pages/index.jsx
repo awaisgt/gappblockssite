280e22814885b9201309bdc710f0a4018f0c7b9e
Hamza Z <hamzafer3@gmail.com>
2022-08-27 18:22:33 +0500

    Initial Web-structure.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,14 @@ import Head from 'next/head'
 
 import { CallToAction } from '@/components/CallToAction'
 import { Faqs } from '@/components/Faqs'
-import { Footer } from '@/components/Footer'
+import Footer from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
 import { Pricing } from '@/components/Pricing'
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
 import { Reviews } from '@/components/Reviews'
 import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+import Stats from "@/components/Stats";
 
 export default function Home() {
   return (
@@ -24,11 +25,8 @@ export default function Home() {
       <main>
         <Hero />
         <SecondaryFeatures />
+        <Stats />
         <PrimaryFeatures />
-        <CallToAction />
-        <Reviews />
-        <Pricing />
-        <Faqs />
       </main>
       <Footer />
     </>
