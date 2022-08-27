5473e37d27ebbbb76b781548f601e2d602ca708e
BahadurAliShah <96332131+BahadurAliShah@users.noreply.github.com>
2022-08-28 01:31:27 +0500

    move navbar to center

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,7 +51,7 @@ export function Header() {
     <header>
       <nav>
         <Container className="relative z-50 flex justify-between py-8">
-          <div className="relative z-10 flex items-center gap-16">
+          <div className="relative flex-1 flex justify-center mr-auto">
             <Link href="/" aria-label="Home">
               <Logo className="h-10 w-auto" />
             </Link>
