190b33f3e448c7efe66d1b81b4c810f18158b055
Awais <awaisghaffar77@gmail.com>
2022-08-28 12:57:51 -0400

    Issue#23 Payment Page updates

---
+++ src/pages/payment.jsx
@@ -0,0 +1,17 @@
+
+import HeroSection from "@/components/HeroSection"
+import { Header } from "@/components/Header"
+import { Footer } from "@/components/Footer"
+import  PreferredPayment  from "@/components/PreferredPayment"
+import PaymentHelp from "@/components/PaymentHelp"
+export default function Payment() {
+
+    return (
+      <>
+      <Header/>
+      <HeroSection/>
+      <PreferredPayment/>
+      <PaymentHelp/>
+     <Footer/>
+      </>
+    )}
