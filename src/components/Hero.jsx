42f5c904e8036a541e1752bb0ac7f21ed368777c
MZO9400 <hamza.hameed00@gmail.com>
2022-08-27 19:00:23 -0500

    Modify hero component
    Address issues pointed out by the design team

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -358,12 +358,12 @@ export function Hero() {
                 href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
                 variant="outline"
               >
-                <span className="ml-2.5">FIND MORE</span>
+                <span className="mx-2.5">FIND MORE</span>
               </Button>
             </div>
           </div>
            <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
-            <div className="-mx-4 h-[448px] px-9 [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-top-10 lg:-bottom-20 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32">
+            <div className="-mx-4 h-[448px] px-9 [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-top-10 md:-bottom-2 lg:h-auto lg:px-0 lg:pt-10">
             <Image
                 src={frame}
                 alt=""
