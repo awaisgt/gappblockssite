9a3abcbebadf5f3ed53ab29d9f8aedb86d8f7559
MZO9400 <hamza.hameed00@gmail.com>
2022-08-27 19:09:03 -0500

    Center align the nav in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,10 +51,10 @@ export function Header() {
     <header>
       <nav>
         <Container className="relative z-50 flex justify-between py-8">
-          <div className="relative z-10 flex items-center gap-16">
             <Link href="/" aria-label="Home">
               <Logo className="h-10 w-auto" />
             </Link>
+          <div className="relative z-10 flex items-center gap-16">
             <div className="hidden lg:flex lg:gap-10">
               <NavLinks />
             </div>
