d8ac1e01ee2a93b8efadff40b1206d1a25f5f0d5
Awais <awaisghaffar77@gmail.com>
2022-08-28 13:19:59 -0400

    Issue#22 Shipping Page updates

---
+++ src/pages/shipping.jsx
@@ -0,0 +1,15 @@
+
+import { Header } from "@/components/Header"
+import { Footer } from "@/components/Footer"
+import PaymentHelp from "@/components/PaymentHelp"
+import ShippingMainCard from "@/components/ShippingMainCard"
+export default function Payment() {
+
+    return (
+      <>
+      <Header/>
+      <ShippingMainCard/>
+      <PaymentHelp/>
+     <Footer/>
+      </>
+    )}
