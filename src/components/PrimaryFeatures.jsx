5fc19c5222eae1a4c62f5be6544e5da5d650e405
Hamza <hamza.hameed00@gmail.com>
2022-08-29 16:30:09 -0500

    Update data for primary features

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -26,21 +26,21 @@ const features = [
   {
     name: 'Storefront',
     description:
-      'We hide your stock purchases behind thousands of anonymous trading accounts, so suspicious activity can never be traced back to you.',
+      'Create stunning e-Commerce apps without a single line of code — giving you full control of your app and its look, feel and functionality.',
     icon: DeviceTouchIcon,
     screen: InvestScreen,
   },
   {
     name: 'Live Tracking',
-    description:
-      'For every friend you invite to Pocket, you get insider notifications 5 seconds sooner. And it’s 10 seconds if you invite an insider.',
+    description: 
+      'Give your customers an Uber-like experience by using GAPP Finder to track their delivery in real-time.',
     icon: DeviceUserIcon,
     screen: InviteScreen,
   },
   {
     name: 'Push Notifications',
     description:
-      'Get a push notification every time we find out something that’s going to lower the share price on your holdings so you can sell before the information hits the public markets.',
+      'Communicate with your customers in real-time instant notifications, your clients will never be unaware of the status of their order.',
     icon: DeviceNotificationIcon,
     screen: StocksScreen,
   }
