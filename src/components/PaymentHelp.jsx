328a9ba86b1efd07ce1fad5f839bdb3e9da5db8c
Afrasyabafzal <afrasyabafzal5@gmail.com>
2022-08-30 11:31:23 -0400

    Modify Shipping Page

--- src/components/PaymentHelp.jsx
+++ src/components/PaymentHelp.jsx
@@ -1,38 +1,68 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid'
+import {
+  SparklesIcon,
+} from '@heroicons/react/24/outline'
 
 export default function PaymentHelp() {
   return (
-    <div className="relative bg-gray-800">
-      <div className="h-56 bg-accent sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
-        <img
-          className="w-full h-full object-cover"
-          src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply"
-          alt=""
-        />
-      </div>
-      <div className="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
-        <div className="md:ml-auto md:w-1/2 md:pl-10">
-          <h2 className="text-lg font-semibold text-gray-300">Award winning support</h2>
-          <p className="mt-2 text-white text-3xl font-bold tracking-tight sm:text-4xl">We’re here to help</p>
-          <p className="mt-3 text-lg text-gray-300">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a
-            scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat quisque ut interdum
-            tincidunt duis.
-          </p>
-          <div className="mt-8">
-            <div className="inline-flex rounded-md shadow">
-              <a
-                href="#"
-                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50"
-              >
-                Visit the help center
-                <ArrowTopRightOnSquareIcon className="-mr-1 ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-              </a>
+    <>
+      <div className="mt-24">
+            <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
+              <div className="mx-auto max-w-xl px-4 sm:px-6 lg:col-start-2 lg:mx-0 lg:max-w-none lg:py-32 lg:px-0">
+                <div>
+                  <div>
+                    <span className="flex h-12 w-12 items-center justify-center rounded-md bg-gradient-to-r from-purple-600 to-indigo-600">
+                      <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    </span>
+                  </div>
+                  <div className="mt-6">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900">
+                      Better understand your customers
+                    </h2>
+                    <p className="mt-4 text-lg text-gray-500">
+                      Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia
+                      porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at.
+                      Lectus viverra dui tellus ornare pharetra.
+                    </p>
+                    <div className="mt-6">
+                      <a
+                        href="#"
+                        className="inline-flex rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
+                      >
+                        Get started
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-12 sm:mt-16 lg:col-start-1 lg:mt-0">
+                <div className="-ml-48 pr-4 sm:pr-6 md:-ml-16 lg:relative lg:m-0 lg:h-full lg:px-0">
+                  <img
+                    className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                    src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
+                    alt="Customer profile user interface"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-indigo-50 mt-20">
+          <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-24 lg:px-8">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
+              <span className="block">Ready to dive in?</span>
+              <span className="block text-indigo-600">Start your free trial today.</span>
+            </h2>
+            <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+              <div className="inline-flex rounded-md shadow">
+                <a
+                  href="#"
+                  className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700"
+                >
+                  Get started
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+        </>
   )
 }
