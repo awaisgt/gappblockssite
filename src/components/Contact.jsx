e00bae5b8a86f5a89ffc78bd743629a7ae3a1d40
MZO9400 <hamza.hameed00@gmail.com>
2022-08-30 20:17:50 -0500

    Connect form spree

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -181,7 +181,7 @@ export function Contact() {
             {/* Contact form */}
             <div className="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
               <h3 className="text-lg font-medium text-gray-900">Send us a message</h3>
-              <form action="#" method="POST" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+              <form action="https://formspree.io/f/mbjbleoj" method="POST" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
                 <div>
                   <label htmlFor="first-name" className="block text-sm font-medium text-gray-900">
                     First name
