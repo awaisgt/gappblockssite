85f1582b50fffe9036cb58bffbb8928f95ee3838
MZO9400 <hamza.hameed00@gmail.com>
2022-09-01 10:22:17 -0500

    Storefront: Content update

--- src/components/StoreFrontHero.jsx
+++ src/components/StoreFrontHero.jsx
@@ -274,6 +274,9 @@ export function StoreFrontHero() {
             <p className="mt-6 text-lg text-gray-600">
               Build beautiful and powerful m-commerce apps without learning to code. GAPP Blocks makes it easy for you to create engaging, user-friendly apps for your customers.
             </p>
+            <p className="mt-6 text-lg text-gray-600">
+              We provide multiple blocks so you can easily integrate your preferred payment methods like Apple Pay and Google Pay, as well as custom extensions for order management and real-time instant notifications. Your users can now track their orders from order submission to delivery using our Uber-like interface.
+            </p>
             <Button href="/pricing" className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
                 GET STARTED
               </Button>
@@ -295,30 +298,6 @@ export function StoreFrontHero() {
                 <AppDemo />
               </PhoneFrame>
             </div>
-          </div>
-          <div className="relative -mt-4 lg:col-span-7 lg:mt-0 xl:col-span-6">
-            <p className="text-center text-sm font-semibold text-gray-900 lg:text-left">
-              Some of our existing customers:
-            </p>
-            <ul
-              role="list"
-              className="mx-auto mt-8 flex max-w-xl flex-wrap justify-center gap-x-10 gap-y-8 lg:mx-0 lg:justify-start"
-            >
-              {[
-                ['Forbes', logoForbes],
-                ['TechCrunch', logoTechcrunch],
-                ['Wired', logoWired],
-                ['CNN', logoCnn, 'hidden xl:block'],
-                ['BBC', logoBbc],
-                ['CBS', logoCbs],
-                ['Fast Company', logoFastCompany],
-                ['HuffPost', logoHuffpost, 'hidden xl:block'],
-              ].map(([name, logo, className]) => (
-                <li key={name} className={clsx('flex', className)}>
-                  <Image src={logo} alt={name} className="h-8" unoptimized />
-                </li>
-              ))}
-            </ul>
           </div>
         </div>
       </Container>
